1c8405336d88b96ab0ef5fc0564e5910b85d26af
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-08-23 20:14:50 -0400

    feat: criação do projeto spring

---
+++ cursospringrest/src/main/java/com/example/cursospringrest/CursospringrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cursospringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringrestApplication.class, args);
+	}
+	
+}
---
+++ cursospringrest/src/test/java/com/example/cursospringrest/CursospringrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursospringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
