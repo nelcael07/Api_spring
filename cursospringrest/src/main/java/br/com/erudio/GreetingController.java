e1d7288a481e6a6a5bd5492f1c7c315c28fc1d53
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2021-08-23 21:38:03 -0400

    feat: começo da implemetação do metodo de soma

--- cursospringrest/src/main/java/br/com/erudio/GreetingController.java
+++ cursospringrest/src/main/java/br/com/erudio/GreetingController.java
@@ -2,8 +2,9 @@ package br.com.erudio;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,11 +20,13 @@ public class GreetingController {
   private final AtomicLong counter = new AtomicLong();
   
   // caminho na url
-  @RequestMapping("/greeting")
-  public Greeting greeting(@RequestParam(value = "name",defaultValue = "word") String Name) {
-      // retorna uma greeting levando os elementos do construtor
-      // incrementAndGet sempre vai incrementar
-      return new Greeting(counter.incrementAndGet(), String.format(template, Name));
+  @RequestMapping(value="/sum/numberOne/numberTwo", method = RequestMethod.GET)
+  // pathVariable torna obrigatorio a passagem da propriedade
+  public double sum(
+    @PathVariable(value = "numberOne") String numberOne, 
+    @PathVariable(value = "numberTwo") String numberTwo
+  ) {
+      return 1D;
   }
   
 }
